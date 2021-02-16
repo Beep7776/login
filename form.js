182dab0d18f850891d6ec5534dc46a765f1f0172
eli <78449268+aeisfu@users.noreply.github.com>
2021-02-16 13:27:26 -0500

    form.js

---
+++ form.js
@@ -0,0 +1,14 @@
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDmAkU2PZ15J0AbuxDBLygY8hq585fDGPo",
+    authDomain: "test-48712.firebaseapp.com",
+    projectId: "test-48712",
+    storageBucket: "test-48712.appspot.com",
+    messagingSenderId: "83883482068",
+    appId: "1:83883482068:web:61e1665ec9d8a3530c5441"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
